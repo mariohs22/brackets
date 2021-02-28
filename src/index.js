973d791d0a4d0ebf6e09fc9e9991d92df14c2e3b
Mariohs22 <serge.burakovsky@gmail.com>
2021-03-01 00:12:45 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let current;
+  const matchLookup = bracketsConfig.reduce(function(result, item, index, array){result[item[0]]=item[1]; return result}, {});
+  const openpair = Object.keys(matchLookup);
+  const closepair = Object.values(matchLookup);
+  for (let i = 0; i < str.length; i++) {
+    current = str[i];
+    if (openpair.includes(current) && closepair.includes(current) && stack.includes(current)){
+      let lastBracket = stack.pop();
+      if (matchLookup[lastBracket] !== current){
+        return false;
+      }
+    } else if (openpair.includes(current)){
+      stack.push(current);
+    } else if (closepair.includes(current)) {
+      let lastBracket = stack.pop();
+      if (matchLookup[lastBracket] !== current){
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
